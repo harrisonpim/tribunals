4867068ff92ae31d6823272d2742119ee64f4610
jamie <j.parkinson@wellcome.org>
2024-06-15 22:26:05 +0100

    Tidying

--- cac/scrape_outcomes.py
+++ cac/scrape_outcomes.py
@@ -1,13 +1,9 @@
-import scrapy
-from scrapy.crawler import CrawlerProcess
-from scrapy.exceptions import NotSupported
 import pymupdf
 import pymupdf4llm
+import scrapy
 from markdownify import markdownify
-
-
-def get_title(response):
-    return response.css("main#content h1::text").get().strip()
+from scrapy.crawler import CrawlerProcess
+from scrapy.exceptions import NotSupported
 
 
 class CacOutcomeSpider(scrapy.Spider):
@@ -20,31 +16,36 @@ class CacOutcomeSpider(scrapy.Spider):
         yield scrapy.Request(
             url=(self.url_prefix + str(self.start_year)),
             callback=self.parse,
-            meta={
-                "year": self.start_year
-            }
+            meta={"year": self.start_year},
         )
 
     def parse(self, response):
-        outcome_links = response.css("h3#trade-union-recognition + div + div > ul > li div a")
+        outcome_links = response.css(
+            "h3#trade-union-recognition + " "div + div > ul > li div a"
+        )
         yield from response.follow_all(
             urls=outcome_links,
             callback=self.parse_outcome,
-            cb_kwargs={"year": response.meta["year"]}
+            cb_kwargs={"year": response.meta["year"]},
         )
 
         maybe_next_year = response.meta["year"] + 1
         yield response.follow(
             url=(self.url_prefix + str(maybe_next_year)),
             callback=self.parse,
-            meta={
-                "year": maybe_next_year
-            }
+            meta={"year": maybe_next_year},
         )
 
     def parse_outcome(self, response, year):
-        decision_title = get_title(response).removeprefix("CAC Outcome: ")
-        reference = response.css("section#documents p::text").re_first(r"Ref:\s*(.+)")
+        decision_title = (
+            response.css("main#content h1::text")
+            .get()
+            .strip()
+            .removeprefix("CAC Outcome: ")
+        )
+        reference = response.css("section#documents p::text").re_first(
+            r"^Ref:\s*(TUR1/\d+\(\d+\)).*"
+        )
         for document in response.css("section#documents > section"):
             outcome_link = document.css("h3 a")
             outcome_title = outcome_link.css("*::text").get().strip()
@@ -55,19 +56,19 @@ class CacOutcomeSpider(scrapy.Spider):
                     "reference": reference,
                     "decision_title": decision_title,
                     "outcome_title": outcome_title,
-                    "year": year
-                }
+                    "year": year,
+                },
             )
 
     def parse_document(self, response, **kwargs):
         try:
             content = self.html_content(response)
         except NotSupported:
-            content = self.pdf_content(response)
-        yield {
-            **kwargs,
-            "content": content.strip()
-        }
+            if response.headers.get("Content-Type").decode() == "application/pdf":
+                content = self.pdf_content(response)
+            else:
+                content = ""
+        yield {**kwargs, "content": content.strip()}
 
     def html_content(self, response):
         content = response.css("main#content div#contents div.govspeak").get().strip()
@@ -82,11 +83,8 @@ if __name__ == "__main__":
     process = CrawlerProcess(
         settings={
             "REQUEST_FINGERPRINTER_IMPLEMENTATION": "2.7",
-            "FEEDS": {
-                "data/outcomes.json": {
-                    "format": "jsonlines"
-                }
-            }
+            "LOG_LEVEL": "INFO",
+            "FEEDS": {"data/outcomes.json": {"format": "jsonlines"}},
         }
     )
     process.crawl(CacOutcomeSpider)
